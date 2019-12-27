c13276b265eccfec9d96891a9fd244752486542e
jpca999 <jpca999@gmail.com>
2019-12-27 09:31:54 -0500

    first commitj

---
+++ simple.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-console,func-names,react/no-multi-comp */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactDragListView from 'react-drag-listview/src/index.js';
+import './index.less';
+
+class Demo extends React.Component {
+  constructor(props) {
+    super(props);
+    const data = [];
+    for (let i = 1, len = 21; i < len; i += 1) {
+      data.push({
+        title: `rows===${i}`
+      });
+    }
+    this.state = {
+      data
+    };
+  }
+
+  render() {
+    const that = this;
+    const dragProps = {
+      onDragEnd(fromIndex, toIndex) {
+        const { data } = that.state;
+        const item = data.splice(fromIndex, 1)[0];
+        data.splice(toIndex, 0, item);
+        that.setState({ data });
+      },
+      nodeSelector: 'li',
+      handleSelector: 'a'
+    };
+
+    return (
+      <div className="simple simple1">
+        <h2>Dragging handle</h2>
+        <div className="simple-inner">
+          <ReactDragListView {...dragProps}>
+            <ol>
+              {this.state.data.map((item, index) => (
+                <li key={index}>
+                  {item.title}
+                  <a href="#">==</a>
+                </li>
+            ))}
+            </ol>
+          </ReactDragListView>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Demo />, document.getElementById('__react-content'));
